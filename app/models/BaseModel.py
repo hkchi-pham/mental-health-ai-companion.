2bbe75dc9fe827cbe39ea6ec529d268854cf99b7
hkchi-pham <khanhchi.phamha@gmail.com>
2025-09-15 09:53:20 +0700

    Create BaseModel.py

---
+++ app/models/BaseModel.py
@@ -0,0 +1,19 @@
+from sqlmodel import SQLModel, Field
+from ..utils import get_current_time, generate_uuid
+from datetime import datetime
+from typing import Optional
+import uuid 
+
+class BaseModel(SQLModel):
+    """
+        Base model for all database models.
+    """
+    __abstract__ = True
+
+    id: str = Field(default_factory=str(uuid.uuid4()), index=True, unique=True, primary_key=True)
+    created_at: str = Field(default_factory=get_current_time)
+    updated_at: Optional[str] = Field(default_factory=get_current_time)
+    deleted_at: Optional[datetime] = Field(default=None, nullable=True)
+    created_by: str = None
+    updated_by: Optional[str] = None
+    
