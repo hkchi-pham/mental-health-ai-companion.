ee04fa45185c2220cce822caf1db5ea11be8aaa6
Hoàng Anh <anhdh0115@gmail.com>
2025-11-10 22:00:59 +0700

    update: add Conversation search

--- app/backend/crud/ConversationCrud.py
+++ app/backend/crud/ConversationCrud.py
@@ -3,30 +3,112 @@ from typing import List, Optional
 from backend.models.ConversationModel import ConversationModel
 from backend.response.ConversationResponse import ConversationCreate, ConversationUpdate
 from backend import utils
+from fastapi.encoders import jsonable_encoder
+from sqlalchemy import func, or_, desc, asc
+from datetime import datetime
+
 
 def create_conversation(session: Session, data: ConversationCreate):
+    print("create convo")
     conv = ConversationModel(**data.dict())
+    print(conv)
     session.add(conv)
+    print(111)
     session.commit()
+    print(123)
     session.refresh(conv)
+    print("done")
     return True
 
 
+def check_conversation_id_exist(db: Session, conv_id: str):
+    conv = db.query(ConversationModel).filter(ConversationModel.conv_id == conv_id).first()
+    print(conv)
+    if conv:
+        return True
+    return False
+
+def search_conversations(
+    session: Session,
+    q: str = None,
+    start_from: str = None,
+    ended_to: str = None,
+    sort_by: str = "created_at",
+    sort_order: str = "asc",
+    page: int =1, page_size = 10
+):
+    print("start search convo")
+    query = session.query(ConversationModel)
+    print("query")
+    if q:
+        query = query.filter(
+            or_(
+                ConversationModel.user_id.ilike(f"%{q}%"),
+                ConversationModel.persona.ilike(f"%{q}%"),
+            )
+        )
+
+    print("q")
+    if start_from:
+        query = query.filter(ConversationModel.created_at >= start_from)
+    if ended_to:
+        query = query.filter(ConversationModel.ended_at <= ended_to)
+    print("filter")
+    if hasattr(ConversationModel, sort_by):
+        column = getattr(ConversationModel, sort_by)
+        query = query.order_by(asc(column) if sort_order.lower() == "asc" else desc(column))
+    else:
+        query = query.order_by(asc(ConversationModel.created_at))
+    print("sort")
+    total = query.count()  
+    
+    statement = query.offset((page - 1) * page_size).limit(page_size).all()
+
+    total_pages = (total + page_size - 1) // page_size if total > 0 else 0
+    print("statement")
+    return {
+        "items": jsonable_encoder(statement),  # ✅ serialize an toàn
+        "total": total,
+        "page": page,
+        "page_size": page_size,
+        "total_pages": total_pages
+    }
+
+
 def get_conversation_by_id(session: Session, conv_id: str):
-    return session.get(ConversationModel, conv_id)
+    print("get conv by id")
+    print(ConversationModel.__table__.columns.keys())
+    conv = session.query(ConversationModel).filter(ConversationModel.id == conv_id, ConversationModel.deleted_at.is_(None)).first()
+    print(conv)
+    if conv:
+        return conv
+    return None
 
 
-def get_all_conversations(session: Session, page: int =1,page_size: int = 10 ):
-    total = session.exec(select(ConversationModel)).count()
+def get_all_conversations(session: Session, page: int = 1,page_size: int = 10 ):
+    print("start to get all convo")
+    stmt = select(func.count()).select_from(ConversationModel).where(
+            ConversationModel.deleted_at.is_(None)
+        )
+    print("stmt")
+    total = session.exec(stmt).first()
+
+    print(total)
+
 
     # lấy danh sách theo phân trang
-    statement = select(ConversationModel).offset((page - 1) * page_size).limit(page_size)
+    statement = (
+        select(ConversationModel)
+        .where(ConversationModel.deleted_at.is_(None))
+        .offset((page - 1) * page_size)
+        .limit(page_size)
+    )
     items = session.exec(statement).all()
 
     total_pages = (total + page_size - 1) // page_size  # làm tròn lên
 
     return {
-        "items": items,
+        "items": jsonable_encoder(items),
         "total": total,
         "page": page,
         "page_size": page_size,
