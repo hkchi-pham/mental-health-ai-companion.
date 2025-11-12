a3b27d2e0fbb43204635d129f92c03a1318aa120
Hoàng Anh <anhdh0115@gmail.com>
2025-11-12 22:07:59 +0700

    fix: fix data type in base model

--- app/backend/models/BaseModel.py
+++ app/backend/models/BaseModel.py
@@ -11,8 +11,8 @@ class BaseModel(SQLModel):
     __abstract__ = True
 
     id: str = Field(default_factory=generate_uuid, index=True, unique=True, primary_key=True)
-    created_at: str = Field(default_factory=get_current_time)
-    updated_at: Optional[str] = Field(default_factory=get_current_time)
+    created_at: datetime = Field(default_factory=datetime.now)
+    updated_at: Optional[datetime] = Field(default_factory=datetime.now)
     deleted_at: Optional[datetime] = Field(default=None, nullable=True)
     created_by: str = None
     updated_by: Optional[str] = None
