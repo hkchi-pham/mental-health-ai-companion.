a64321d8059f83444a6e522e88cdbc3ed890bebd
Hoàng Anh <anhdh0115@gmail.com>
2025-11-12 22:09:09 +0700

    update: add Token data in AuthResponse

--- app/backend/response/AuthResponse.py
+++ app/backend/response/AuthResponse.py
@@ -1,8 +1,14 @@
 from sqlmodel import SQLModel
 from datetime import time
+from typing import Optional
 
 class AuthToken(SQLModel):
     access_token: str
     expires_in: time 
     user_name: str
     user_id: str
+
+
+class TokenData(SQLModel):
+    username: Optional[str] = None
+    user_id: Optional[int] = None
